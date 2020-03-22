b323e8538cd2ec365ace4ee1ef04d306ff2584ef
Antonio Carlos <antonioscheffe@gmail.com>
2020-03-22 18:40:23 -0300

    Aulas de Java

---
+++ Aulas Java Iniciante/Primeira Aula/Antonio.java
@@ -0,0 +1,8 @@
+public class Antonio{
+	public static void main (String[] args){
+        	System.out.println("teste1");
+		for (int i = 0; i <=10; i++){ 
+			System.out.println("Teste Antonio");
+		}
+	}
+}
